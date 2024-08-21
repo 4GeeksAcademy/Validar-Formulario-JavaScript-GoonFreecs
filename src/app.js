4c6f7afaa108eff21fef272fc5bbae2343ad8984
Gonzalo Cortés <gonzaloigcortescortes@gmail.com>
2024-08-21 22:05:32 +0000

    Avanzando

--- src/app.js
+++ src/app.js
@@ -9,3 +9,16 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let card = document.querySelector("#cardNumber");
+let cvc = document.querySelector("#cvc");
+let amount = document.querySelector("#amount");
+let firstName = document.querySelector("#firstName");
+let lastname = document.querySelector("#lastname");
+let city = document.querySelector("#city");
+let state = document.querySelector("#state");
+let Postalcode = document.querySelector("#Postal-code");
+/*let card = document.querySelector("#cardNumber")*/
+let message = document.querySelector("#message");
+let submit = document.querySelector(".btn btn-primary");
+let alertaError = document.querySelector("#cardNumber");
